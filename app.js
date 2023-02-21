58a0d395ee9dd86156fdfa0e1de6372c293ee3ad
Nathan Pedigo <njpedigo@gmail.com>
2023-02-21 10:14:39 -0700

    working with api's

---
+++ app.js
@@ -0,0 +1,9 @@
+const bid1Btn = document.getElementById('bid1');
+const bid2Btn = document.getElementById('bid2');
+
+bid1Btn.addEventListener('click', handleOnClick);
+bid2Btn.addEventListener('click', handleOnClick);
+
+function handleOnClick() {
+    
+}
